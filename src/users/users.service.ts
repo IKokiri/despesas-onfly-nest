9e14f8c76242eeebd55d0e71b5e3f1b9dd0b95ca
Luiz Mendes <luiz.mendes@devnology.com.br>
2023-07-30 23:39:03 -0300

    feat: create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+
   create(createUserDto: CreateUserDto) {
+    this.usersRepository.save(createUserDto)
     return 'This action adds a new user';
   }
 
