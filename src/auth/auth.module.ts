d51bb7b7e4778060f8f18c2822157ae831414e48
Luiz Mendes <luiz.mendes@devnology.com.br>
2023-08-01 02:42:16 -0300

    refactor: add direct secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
-import { jwtConstants } from './constants';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: process.env.SECRET,
       signOptions: { expiresIn: '3600s' },
     }),
   ],
