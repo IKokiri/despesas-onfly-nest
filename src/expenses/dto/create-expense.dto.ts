a0744170cae2c2c7d8513180b6caa862a405e028
Luiz Mendes <luiz.mendes@devnology.com.br>
2023-08-01 04:01:09 -0300

    feat: validate expense data

--- src/expenses/dto/create-expense.dto.ts
+++ src/expenses/dto/create-expense.dto.ts
@@ -1,6 +1,19 @@
+import { IsDate, IsDateString, IsInt, IsNotEmpty, IsNumber, IsPositive, Length } from "class-validator"
+import { IsNotDateFuture } from "src/validator/IsNotDateFuture"
+
 export class CreateExpenseDto {
+    @Length(0, 191)
     description: string
+    
+    @IsDateString()
+    @IsNotDateFuture()
     expenseDate: Date
+
+    @IsInt()
+    @IsNotEmpty()
     userId: number
+
+    @IsNumber()
+    @IsPositive()
     amount: number
 }
